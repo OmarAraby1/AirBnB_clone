14144c0b8805e400fd958fc798ab743373cf0a81
Omar Araby <omararaby2001@gmail.com>
2024-03-10 18:23:56 -0700

    Task1

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class for all model classes."""
+
+    def __init__(self):
+        """Initializes a BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a formatted string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the updated_at attribute to the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the instance."""
+        data = self.__dict__.copy()
+        data["__class__"] = self.__class__.__name__
+        data["created_at"] = self.created_at.isoformat()
+        data["updated_at"] = self.updated_at.isoformat()
+        return data
